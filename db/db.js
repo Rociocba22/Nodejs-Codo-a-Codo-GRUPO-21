4e650eafea04abcb2f6460cff288a52bee2e1e51
UriCapdevila <Uri_CapdevilaWRK@outlook.com>
2024-07-02 20:41:15 -0300

    Cambios al DB y creacion del env

--- db/db.js
+++ db/db.js
@@ -1,11 +1,21 @@
+require('dotenv').config();
+
+
 const mySql = require('mysql2');
 
 const connection = mySql.createConnection(
-    {
+    {   /*
         host : 'localhost',
         user: 'root',
         password : 'root',
         database: 'grupo21'
+        */
+
+        host : process.env.DB_Host ,
+        user: process.env.DB_Usuario,
+        password : process.env.DB_Password,
+        database: process.env.DB_Name
+
     });
 
 
