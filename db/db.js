422ab87bf2cc1510985a12ec4c26e1131c222af0
Rociocba22 <rociocba05@gmail.com>
2024-07-03 16:56:08 -0300

    archivos de vercel

--- db/db.js
+++ db/db.js
@@ -4,33 +4,57 @@ require('dotenv').config();
 const mySql = require('mysql2');
 
 const connection = mySql.createConnection(
-    {   /*
-        host : 'localhost',
-        user: 'root',
-        password : 'root',
-        database: 'grupo21'
-        */
-
-        /* host : process.env.DB_Host ,
-        user: process.env.DB_Usuario,
-        password : process.env.DB_Password,
-        database: process.env.DB_Name */
-
-    });
-
-
-    connection.connect((err) => 
     {
-        if(err)
-        {
-            console.error("ERROR conectando a la base de datos", err);
-            return;
-        }
+        // host: 'localhost',
+        // user: 'root',
+        // password: 'root',
+        // database: 'grupo21'
+       
 
-        console.log("Conectado EXITOSAMENTE a la base de datos");
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSORD,
+        database: process.env.DB_NAME
 
     });
+
+
+connection.connect((err) => {
+    if (err) {
+        console.error("ERROR conectando a la base de datos", err);
+        return;
+    }
+
+    console.log("Conectado EXITOSAMENTE a la base de datos");
+
+});
 
 
 module.exports = connection;
+
+// const mySql = require('mysql2');
+
+// const connection = mySql.createConnection(
+//     {
+//         host : 'localhost',
+//         user: 'root',
+//         password : 'root',
+//         database: 'grupo21'
+//     });
+
+
+//     connection.connect((err) =>
+//     {
+//         if(err)
+//         {
+//             console.error("ERROR conectando a la base de datos", err);
+//             return;
+//         }
+
+//         console.log("Conectado EXITOSAMENTE a la base de datos");
+
+//     });
+
+
+// module.exports = connection;
 
